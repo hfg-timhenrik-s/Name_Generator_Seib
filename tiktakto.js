f0dd0ce9d366a865a54c517da6e2bf7173d65285
hfg-timhenrik-s <72493293+hfg-timhenrik-s@users.noreply.github.com>
2020-12-15 14:15:35 +0100

    Logik

--- tiktakto.js
+++ tiktakto.js
@@ -55,7 +55,37 @@ function change() {
 
 function clickPlayer(id) {
     document.getElementById(id).style.backgroundColor=colourChange()
-    change()
+    // Zellen Id mit Spielfeld verbinden
+    if(id === "ZelleEins") {
+        field[0][0] = aktuellerSpieler
+    }
+    else if(id === "ZelleZwei") {
+        field[0][1] = aktuellerSpieler
+    }
+    else if(id === "ZelleDrei") {
+        field[0][2] = aktuellerSpieler
+    }
+    else if(id === "ZelleVier") {
+        field[1][0] = aktuellerSpieler
+    }
+    else if(id === "ZelleFünf") {
+        field[1][1] = aktuellerSpieler
+    }
+    else if(id === "ZelleSechs") {
+        field[1][2] = aktuellerSpieler
+    }
+    else if(id === "ZelleSieben") {
+        field[2][0] = aktuellerSpieler
+    }
+    else if(id === "ZelleAcht") {
+        field[2][1] = aktuellerSpieler
+    }
+    else if(id === "ZelleNeun") {
+        field[2][2] = aktuellerSpieler
+    }
+    WinCheck();
+    change();
+    console.log(field);
 }
 
 
@@ -71,7 +101,27 @@ function colourChange() {
 
 //Spielstand verstehen
 
-// zufallszahl 
+//Gewinnbedingung
+
+function WinCheck() {
+    // schleife überprüft das Spielfeld
+    for( let i = 0; i < 3 ;i++) {
+        // horizontale Gewinnmöglichkeiten
+        if(field[i][0] + field[i][1] + field[i][2] === playerOne + playerOne + playerOne || field[i][0] + field[i][1] + field[i][2] === playerTwo + playerTwo + playerTwo) {
+            alert(field[i][0] + "won");
+        }
+        else if(field[0][i] + field[1][i] + field[2][i] === playerOne + playerOne + playerOne || field[0][i] + field[1][i] + field[2][i] === playerTwo + playerTwo + playerTwo) {
+            alert(field[i][0] + "won"); 
+        }
+    } if(field[0][0] + field[1][1] + field[2][2] === playerOne + playerOne + playerOne || field[0][0] + field[1][1] + field[2][2] === playerTwo + playerTwo + playerTwo) {
+        alert(field[0][0] + "won"); 
+    } if(field[0][2] + field[1][1] + field[2][0] === playerOne + playerOne + playerOne || field[0][2] + field[1][1] + field[2][0] === playerTwo + playerTwo + playerTwo) {
+        alert(field[0][2] + "won"); 
+    }
+}
+
+
+// Zufallszahl 
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
